8f8226b7cb856f179fd5982d6ae90ffca2b27e1f
Viktor Tovstyi <viktor.tovstyi@gmail.com>
2025-06-24 13:13:37 +0300

    added assembly, fixed recipes location.

--- src/main/java/com/ganteater/ae/processor/Python.java
+++ src/main/java/com/ganteater/ae/processor/Python.java
@@ -1,21 +1,26 @@
 package com.ganteater.ae.processor;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.Function;
 
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
 
 import com.ganteater.ae.CommandException;
+import com.ganteater.ae.util.AEUtils;
 import com.ganteater.ae.util.xml.easyparser.Node;
 
 public class Python extends BaseProcessor {
 
+	static {
+		System.setProperty("python.import.site", "false");
+	}
+
 	public final class LogOut extends PyObject {
 		private static final long serialVersionUID = -121688163044757100L;
-		
-		public boolean softspace = false; 
 
+		public boolean softspace = false;
 		private Function<String, Object> func;
 
 		public LogOut(Function<String, Object> func) {
@@ -33,10 +38,6 @@ public class Python extends BaseProcessor {
 		}
 	}
 
-	static {
-		System.setProperty("python.import.site", "false");
-	}
-
 	@Override
 	public void init(Processor aParent, Node action) throws CommandException {
 		super.init(aParent, action);
@@ -45,8 +46,13 @@ public class Python extends BaseProcessor {
 			interpreter.setErr(new LogOut((m) -> log.error(m)));
 			String fileName = action.getAttribute("file");
 			if (fileName != null) {
-				File file = getFile(fileName);
-				interpreter.execfile(file.getAbsolutePath());
+				InputStream inputStream;
+				try {
+					inputStream = AEUtils.getInputStream(fileName, getBaseDir());
+					interpreter.execfile(inputStream);
+				} catch (IOException e) {
+					throw new CommandException(e, this, action);
+				}
 			}
 			String innerText = action.getInnerText();
 			if (!innerText.isEmpty()) {
