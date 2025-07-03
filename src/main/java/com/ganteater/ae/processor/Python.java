b9f6a168e69491888075c7996f997185217f3d16
Viktor Tovstyi <viktor.tovstyi@gmail.com>
2025-07-03 15:25:19 +0300

    improved Python processor.

--- src/main/java/com/ganteater/ae/processor/Python.java
+++ src/main/java/com/ganteater/ae/processor/Python.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.function.Function;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
 
 import com.ganteater.ae.CommandException;
+import com.ganteater.ae.processor.annotation.CommandExamples;
 import com.ganteater.ae.util.AEUtils;
 import com.ganteater.ae.util.xml.easyparser.Node;
 
@@ -38,27 +41,50 @@ public class Python extends BaseProcessor {
 		}
 	}
 
+	private PythonInterpreter interpreter;
+
 	@Override
 	public void init(Processor aParent, Node action) throws CommandException {
 		super.init(aParent, action);
-		try (PythonInterpreter interpreter = new PythonInterpreter()) {
-			interpreter.setOut(new LogOut((m) -> log.info(m)));
-			interpreter.setErr(new LogOut((m) -> log.error(m)));
-			String fileName = action.getAttribute("file");
+		interpreter = new PythonInterpreter();
+		interpreter.setOut(new LogOut((m) -> log.info(m)));
+		interpreter.setErr(new LogOut((m) -> log.error(m)));
+		String innerText = action.getInnerText();
+		IOException fileError = null;
+		String fileName = action.getAttribute("file");
+		if (StringUtils.isEmpty(innerText)) {
 			if (fileName != null) {
 				InputStream inputStream;
 				try {
 					inputStream = AEUtils.getInputStream(fileName, getBaseDir());
-					interpreter.execfile(inputStream);
+					innerText = IOUtils.toString(inputStream);
 				} catch (IOException e) {
-					throw new CommandException(e, this, action);
+					fileError = e;
 				}
 			}
-			String innerText = action.getInnerText();
-			if (!innerText.isEmpty()) {
-				interpreter.exec(innerText);
-			}
+		}
+		innerText = replaceProperties(innerText);
+		if (!innerText.isEmpty()) {
+			interpreter.exec(innerText);
+		} else if (fileName != null && fileError != null) {
+			throw new CommandException(fileError, this, action);
 		}
 	}
 
+	@CommandExamples({ "<Run name='type:property'> ... </Run>" })
+	public void runCommandRun(Node command) {
+		String innerText = command.getInnerText();
+		innerText = replaceProperties(innerText);
+		interpreter.exec(innerText);
+		String name = command.getAttribute("name");
+		if (name != null) {
+			setVariableValue(name, interpreter.get(name));
+		}
+	}
+
+	@Override
+	public void stop() {
+		super.stop();
+		interpreter.close();
+	}
 }
